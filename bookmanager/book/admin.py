792fb1841c9fab68e8263e0b3e47021f0811389f
Jonas.Mao <Jonas.Mao@163.com>
2024-03-27 08:56:34 +0800

    v4

--- bookmanager/book/admin.py
+++ bookmanager/book/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from book.models import BookInfo,PeopleInfo
 
-#注册模型类(注册后重启)
+##注册模型类(注册后重启)
 admin.site.register(BookInfo)
 admin.site.register(PeopleInfo)
